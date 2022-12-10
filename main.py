b1128db09549d6877a4146b55ffc3d9901dc882e
guigonzalez <guidogermang@gmail.com>
2022-12-10 20:48:12 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+from google.cloud import bigquery
+import urllib
+from bs4 import BeautifulSoup
+import yfinance as yf
+
+CREDS_BIGQUERY = '/creds/bigsurmining-14baacf42c48.json'
+
+#######UPDATE BTC
+def getBtcValue():
+    BTC_Ticker = yf.Ticker("BTC-USD")
+    BTC_Data = BTC_Ticker.history(period="1D")
+    BTC_Value = BTC_Data['High'].loc[BTC_Data.index[0]]
+    return BTC_Value
+
+
+#UPDATE USD
+def getUSDValue():
+    url = "https://dolarhoy.com/cotizaciondolarblue"
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html,"html.parser")
+    tags = soup.find_all("div",class_="value")
+    precios = list()
+    for tag in tags:
+        precios.append(tag.contents[0])
+    buy = float(precios[0].replace("$",""))
+    sell = float(precios[1].replace("$",""))
+    return buy
+
+
+def zabbix_push(puid, key, value):
+    stream = os.popen(f"zabbix_sender -z '54.92.215.92'    -s {puid} -k application.{key} -o {str(value)}")
+    output = stream.read()
+    #print(output)
+
+def bigQueryRead(query):
+    client = bigquery.Client.from_service_account_json(json_credentials_path=CREDS_BIGQUERY)
+    bq_response = client.query(query=f'{query}').to_dataframe()
+    return bq_response
+
+usuariosDF = bigQueryRead(f"SELECT * FROM BD1.usuarios ORDER BY id DESC")
+for usuariosPool, inversionInicial,revShare,revShare_std, totalMined_mtd, actualHashrate, qtyAsics,activeWorkers,inactiveWorkers,totalMined_std,totalPayed_mtd,inmatureBalance,totalPayed_std,revShare_mtd,paidTodayEstimate in zip(usuariosDF["usuariosPool"],usuariosDF["inversionInicial"],usuariosDF["revShare"], usuariosDF["revShare_std"], usuariosDF["totalMined_mtd"], usuariosDF["actualHashrate"], usuariosDF["qtyAsics"],usuariosDF["activeWorkers"],usuariosDF["inactiveWorkers"],usuariosDF["totalMined_std"],usuariosDF["totalPayed_mtd"],usuariosDF["inmatureBalance"],usuariosDF["totalPayed_std"],usuariosDF["revShare_mtd"],usuariosDF["paidTodayEstimate"]):
+    zabbix_push(usuariosPool, "dolar_blue", getUSDValue())
+    zabbix_push(usuariosPool, "btc_price", getBtcValue())
+    zabbix_push(usuariosPool, "valor_kwh", 11)
+    zabbix_push(usuariosPool, "inversion_inicial", inversionInicial)
+    zabbix_push(usuariosPool, "rev_share", revShare)
+    zabbix_push(usuariosPool, "revShare_std", revShare_std)
+    zabbix_push(usuariosPool, "totalMined_mtd", totalMined_mtd)
+    zabbix_push(usuariosPool, "hashrate", actualHashrate)
+    zabbix_push(usuariosPool, "qtyAsics", qtyAsics)
+    zabbix_push(usuariosPool, "workers_active", activeWorkers)
+    zabbix_push(usuariosPool, "workers_inactive", inactiveWorkers)
+    zabbix_push(usuariosPool, "totalMined_std", totalMined_std)
+    zabbix_push(usuariosPool, "totalPayed_mtd", totalPayed_mtd)
+    zabbix_push(usuariosPool, "inmatureBalance", inmatureBalance)
+    zabbix_push(usuariosPool, "totalPayed_std", totalPayed_std)
+    zabbix_push(usuariosPool, "revShare_mtd", revShare_mtd)
+    zabbix_push(usuariosPool, "paidTodayEstimate", paidTodayEstimate)
